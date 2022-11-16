0d839ef490cd416ef16dccd1b3ecb8a73f82308b
wonde-16 <ariana.vilches.88@eaceuta.com>
2022-11-16 14:01:41 +0100

    16/11/2022

---
+++ SILOCREATIVO/js/menu.js
@@ -0,0 +1,6 @@
+// const burguer = document.getElementsById("burguer");  
+
+// console.log(burguer[0]);     
+// console.log(burguer.length);  
+
+const burguer = document.getElementsById("burguer");  

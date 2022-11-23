a131e63239ce785b33bda616367ea988d56c4e77
wonde-16 <ariana.vilches.88@eaceuta.com>
2022-11-23 13:37:52 +0100

    23/11/2022

--- SILO/js/estilo.js
+++ SILO/js/estilo.js
@@ -1,34 +1,38 @@
 
+// const imagenSilo = document.querySelector(".imagen");
+
+// console.log(imagenSilo);
+
+// console.log(imagenSilo.getAttribute("src"));
+
+// imagenSilo.setAttribute("src", "img/logo-Google.png")
+
 
 
 const icono_burger = document.getElementById("burger");
-
 // console.log(icono_burger);
+
+const menu = document.querySelector(".menu");
 
 icono_burger.addEventListener("click", 
       function(){
 
-      const menu = document.querySelector(".menu");
-
       // console.log(menu.classList);
+       // menu.classList.add("nuevo")
+       // menu.classList.remove("menu")
+       // console.log(menu.classList);
+       //  console.log(menu.classList.contains("nuevo"));
 
-      // menu.classList.add("nuevo")
-      // menu.classList.remove("menu")
+       if (menu.classList.contains("ocultar") == true){
+         menu.classList.remove("ocultar")
+         menu.classList.add("mostrar")
+       }
+       else if (menu.classList.contains("mostrar") == true){
+           menu.classList.remove("mostrar")
+           menu.classList.add("ocultar")
+       }
 
-      // console.log(menu.classList);
+       // div.classList.replace("ocultar", "mostrar");    
+       // div.classList.replace("mostrar", "ocultar"); 
+     });
 
-
-      //  console.log(menu.classList.contains("nuevo"));
-
-      if (menu.classList.contains("nuevo") == true){
-        menu.classList.remove("nuevo")
-        menu.classList.add("menu")
-      }
-      else if (menu.classList.contains("menu") == true){
-          menu.classList.remove("menu")
-          menu.classList.add("nuevo")
-      }
-
-      // div.classList.replace("ocultar", "mostrar");    
-      // div.classList.replace("mostrar", "ocultar"); 
-    });

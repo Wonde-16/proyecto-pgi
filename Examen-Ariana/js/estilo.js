c00ef79d87d945520a5955b93ad3ec6570dab1f0
wonde-16 <ariana.vilches.88@eaceuta.com>
2022-11-24 11:09:20 +0100

    fgt

---
+++ Examen-Ariana/js/estilo.js
@@ -0,0 +1,17 @@
+const modalD = document.getElementById("modal");
+const cora = document.querySelector(".cora");
+
+modalD.addEventListener("click", 
+      function(){
+
+       if (cora.classList.contains("cora1") == true){
+         cora.classList.remove("cora1")
+         cora.classList.add("cora2")
+       }
+       else if (cora.classList.contains("cora2") == true){
+           cora.classList.remove("cora2")
+           cora.classList.add("cora1")
+       }
+
+    
+     });

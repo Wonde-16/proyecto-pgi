3d4f2393a3eb885cbbb38f5b5da64659c5e93fd9
Wonde-16 <115214551+Wonde-16@users.noreply.github.com>
2022-12-19 23:48:00 +0100

    19/12/22

--- github/js/estilo.js
+++ github/js/estilo.js
@@ -1,16 +1,19 @@
+const tag = document.getElementById("corazon");
+// console.log(tag);
+
 const cora = document.querySelector(".cora");
-console.log(cora)
+// console.log(cora);
+
 tag.addEventListener("click", 
-      function(){
+       function(){
+            console.log("hola");
 
-       if (cora.classList.contains("cora1") == true){
-         cora.classList.remove("cora1")
-         cora.classList.add("cora2")
-       }
-       else if (cora.classList.contains("cora2") == true){
-           cora.classList.remove("cora2")
-           cora.classList.add("cora1")
-       }
-
-    
-     });
+            if (cora.classList.contains("cora1") == true){
+                cora.classList.remove("cora1")
+                cora.classList.add("cora2")
+             }
+                else if (cora.classList.contains("cora2") == true){
+                cora.classList.remove("cora2")
+                cora.classList.add("cora1")
+                }
+      });

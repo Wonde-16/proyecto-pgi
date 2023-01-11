63b701c4dec8eb1eb71cb03acd09790d183acbcd
wonde-16 <ariana.vilches.88@eaceuta.com>
2023-01-11 14:00:03 +0100

    11/01/2023

---
+++ SILO/js/modoOscuro.js
@@ -0,0 +1,11 @@
+const boton = document.getElementById("chk")
+console.log(boton)
+
+boton.addEventListener("click", 
+    function(){
+        document.body.classList.toggle("modoOscuro")
+   });
+
+
+
+
